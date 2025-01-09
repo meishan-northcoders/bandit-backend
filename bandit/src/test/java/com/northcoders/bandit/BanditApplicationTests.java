e1d6b0c0009abdb67dbc62abb305f873b38b042e
Abulaf1a <peterrazzell688@gmail.com>
2025-01-09 09:31:10 +0000

    initial commit

---
+++ bandit/src/test/java/com/northcoders/bandit/BanditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.bandit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
