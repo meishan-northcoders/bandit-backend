19055383638e1ebdf85462b4e51ec66818a37c7f
samprice <samprice101@hotmail.co.uk>
2025-01-09 15:45:10 +0000

    feat: implemen MessageService interface, declaring methods required in Message service layer implementations

---
+++ src/main/java/com/northcoders/bandit/service/MessageService.java
@@ -0,0 +1,13 @@
+package com.northcoders.bandit.service;
+
+import com.northcoders.bandit.model.Message;
+import com.northcoders.bandit.model.MessageDTO;
+
+import java.util.List;
+
+public interface MessageService {
+
+    Message saveMessage(MessageDTO message);
+
+    List<Message> getAllMessagesBetweenUsers (Long userId1, Long userId2);
+}
