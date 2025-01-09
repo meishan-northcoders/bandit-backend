90d0d003c55e39648befdce6314c3293cce7eae1
samprice <samprice101@hotmail.co.uk>
2025-01-09 16:46:40 +0000

    feat: implement findBySenderIdAndReceiverId method in MessageRepository

--- src/main/java/com/northcoders/bandit/repository/MessageRepository.java
+++ src/main/java/com/northcoders/bandit/repository/MessageRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface MessageRepository extends CrudRepository<Message, Long> {
-    List<Message> findBySenderIdAndReceiverId(Long SenderId, Long ReceiverId);
+public interface MessageRepository extends CrudRepository<Message, String> {
+    List<Message> findBySenderIdAndReceiverId(String SenderId, String ReceiverId);
 }
