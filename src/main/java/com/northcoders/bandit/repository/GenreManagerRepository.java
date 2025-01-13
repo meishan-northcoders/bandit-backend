a600cbe796c5ce8b226ad82ead01ebc1497abfad
Abulaf1a <peterrazzell688@gmail.com>
2025-01-13 10:24:48 +0000

    refactor: deleted unused genre and instrument enums

--- src/main/java/com/northcoders/bandit/repository/GenreManagerRepository.java
+++ src/main/java/com/northcoders/bandit/repository/GenreManagerRepository.java
@@ -1,7 +1,6 @@
 package com.northcoders.bandit.repository;
 
 import com.northcoders.bandit.model.Genre;
-import com.northcoders.bandit.model.GenreType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/northcoders/bandit/repository/InstrumentManagerRepository.java
+++ src/main/java/com/northcoders/bandit/repository/InstrumentManagerRepository.java
@@ -1,6 +1,5 @@
 package com.northcoders.bandit.repository;
 
-import com.northcoders.bandit.model.InstType;
 import com.northcoders.bandit.model.Instrument;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
